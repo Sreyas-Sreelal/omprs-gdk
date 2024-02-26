b70f8b0fa8e460490862fe5f21710f94ee286fb2
SyS <sreyassreelal@gmail.com>
2024-02-26 08:35:04 +0530

    somewhat better way to convert C allocated string to Rust  string.

--- omprs-gdk/src/players.rs
+++ omprs-gdk/src/players.rs
@@ -1,12 +1,10 @@
-use std::{
-    ffi::c_char,
-    str::from_utf8,
-};
+use std::ffi::{c_char, CStr};
 
 pub static mut OMPRS_SendClientMessage: Option<unsafe extern "C" fn(isize, isize, *const c_char)> =
     None;
 
-pub static mut OMPRS_GetPlayerName: Option<unsafe extern "C" fn(isize, *mut u8) -> isize> = None;
+pub static mut OMPRS_GetPlayerName: Option<unsafe extern "C" fn(isize, *mut c_char) -> isize> =
+    None;
 
 pub fn SendClientMessage(playerid: isize, colour: isize, message: &str) -> isize {
     unsafe { OMPRS_SendClientMessage.unwrap()(playerid, colour, cstr!(message)) }
@@ -14,16 +12,11 @@ pub fn SendClientMessage(playerid: isize, colour: isize, message: &str) -> isize
 }
 
 pub fn GetPlayerName(playerid: isize, name: &mut String) -> isize {
-    let mut addr = vec![0u8; 25];
+    let mut addr = vec![0 as c_char; 25];
+
     let length = unsafe { OMPRS_GetPlayerName.unwrap()(playerid, addr.as_mut_ptr()) };
 
-    // probably not the best way to do this, need more research
-    let addr: Vec<u8> = addr
-        .to_vec()
-        .iter()
-        .filter_map(|x| if *x != b'\0' { Some(*x) } else { None })
-        .collect();
-    *name = from_utf8(&addr).unwrap().to_owned();
+    *name = unsafe { CStr::from_ptr(addr.as_ptr()).to_string_lossy().to_string() };
 
     length
 }
