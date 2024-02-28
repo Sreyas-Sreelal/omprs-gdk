379c767cfe0342b2727161d961a312e60c5a1f75
SyS <sreyassreelal@gmail.com>
2024-02-28 08:54:25 +0530

    wrote linux's library loading code

--- omprs-gdk/src/helper.rs
+++ omprs-gdk/src/helper.rs
@@ -1,10 +1,20 @@
 use std::{ffi::CString, iter};
 
-// TODO: write Linux code too
 use windows::{
     core::{PCSTR, PCWSTR},
     Win32::System::LibraryLoader::{GetModuleHandleW, GetProcAddress},
 };
+
+#[cfg(target_os = "linux")]
+use std::os::raw;
+
+#[cfg(target_os = "linux")]
+#[cfg(link(name = "dl"))]
+extern "C" {
+    fn dlopen(filename: *const raw::c_char, flags: raw::c_int) -> *mut raw::c_void;
+    fn dlsym(handle: *mut raw::c_void, symbol: *const raw::c_char) -> *mut raw::c_void;
+    fn dlerror() -> *mut raw::c_char;
+}
 
 macro_rules! cstr {
     ($e: expr) => {{
@@ -31,8 +41,16 @@ pub fn get_module_symbol_address(module: &str, symbol: &str) -> Option<usize> {
         .chain(iter::once(0))
         .collect::<Vec<u16>>();
     let symbol = CString::new(symbol).unwrap();
+
+    #[cfg(target_os = "windows")]
     unsafe {
         let handle = GetModuleHandleW(PCWSTR(module.as_ptr() as _)).unwrap();
         GetProcAddress(handle, PCSTR(symbol.as_ptr() as _)).map(|func| func as usize)
+    }
+
+    #[cfg(target_os = "linux")]
+    unsafe {
+        let handle = dlopen("omprs",1);
+        dlsym(handle, symbol.as_ptr() as _) as usize
     }
 }
