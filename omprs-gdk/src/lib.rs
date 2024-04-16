5e09fa7d0d999ef0163c9f28f9f81ea4666bcc3b
SyS <sreyassreelal@gmail.com>
2024-04-16 21:41:07 +0530

    structure function exports and implementations separately

--- omprs-gdk/src/lib.rs
+++ omprs-gdk/src/lib.rs
@@ -7,24 +7,28 @@ mod helper;
 mod types;
 
 mod actors;
+mod functions;
 mod models;
+mod objects;
 mod players;
 mod utils;
+mod vehicles;
+
+use functions::{actors::load_actor_functions, players::load_player_functions};
 
 pub use crate::utils::*;
 pub use actors::*;
-pub use models::*;
+//pub use models::*;
+pub use objects::*;
 pub use omprs_codegen::{callback, main, native};
 pub use players::*;
 pub use types::*;
+pub use vehicles::*;
 
 pub fn init_functions() {
-    load_function!(SendClientMessage);
     load_function!(Print);
-    load_function!(GetPlayerName);
-
     // models
-    load_function!(AddCharModel);
+    /* load_function!(AddCharModel);
     load_function!(AddSimpleModel);
     load_function!(AddSimpleModelTimed);
     load_function!(GetPlayerCustomSkin);
@@ -32,7 +36,7 @@ pub fn init_functions() {
     load_function!(FindModelFileNameFromCRC);
     load_function!(FindTextureFileNameFromCRC);
     load_function!(IsValidCustomModel);
-    load_function!(GetCustomModelPath);
+    load_function!(GetCustomModelPath); */
 
     load_player_functions();
     load_actor_functions();
