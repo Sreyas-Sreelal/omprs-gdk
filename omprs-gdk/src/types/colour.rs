621d359e9a3224018ae40ec5f0189044a7c579b9
SyS <sreyassreelal@gmail.com>
2024-04-16 21:38:50 +0530

    add fromRgba for Colour

--- omprs-gdk/src/types/colour.rs
+++ omprs-gdk/src/types/colour.rs
@@ -1,16 +1,31 @@
+#[repr(C)]
+struct RGBA {
+    r: u8,
+    g: u8,
+    b: u8,
+    a: u8,
+}
+#[repr(C)]
+union ColourData {
+    rgba: std::mem::ManuallyDrop<RGBA>,
+}
 
 #[repr(C)]
-struct RGBA{
-    r:u8,
-    g:u8,
-    b:u8,
-    a:u8,
-}
-#[repr(C)]
-union  ColourData{
-    rgba:std::mem::ManuallyDrop<RGBA>
-}
-#[repr(C)]
 pub struct Colour {
-    data:ColourData 
+    data: ColourData,
+}
+
+impl Colour {
+    pub fn from_rgba(from: u32) -> Colour {
+        Colour {
+            data: ColourData {
+                rgba: std::mem::ManuallyDrop::new(RGBA {
+                    r: ((from & 0xFF000000) >> 24) as u8,
+                    g: ((from & 0x00FF0000) >> 16) as u8,
+                    b: ((from & 0x0000FF00) >> 8) as u8,
+                    a: (from & 0x000000FF) as u8,
+                }),
+            },
+        }
+    }
 }
