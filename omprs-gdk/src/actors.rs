a8fd720fd25c9fec96ff7d4f5563fcae53aa662d
SyS <sreyassreelal@gmail.com>
2024-04-18 11:28:28 +0530

    add actor methods

--- omprs-gdk/src/actors.rs
+++ omprs-gdk/src/actors.rs
@@ -1,4 +1,7 @@
-use crate::types::vector::Vector3;
+use crate::animationdata::AnimationData;
+use crate::players::Player;
+use crate::{functions, types::vector::Vector3};
+
 use std::ffi::c_void;
 
 pub struct Actor {
@@ -13,9 +16,139 @@ impl Actor {
     pub fn new(handle: *const c_void) -> Self {
         Self { handle }
     }
+
+    pub fn create_actor(skin: isize, pos: Vector3, angle: f32) -> Actor {
+        functions::actors::CreateActor(skin, pos, angle)
+    }
+
+    pub fn destroy(&self) {
+        functions::actors::DestroyActor(self)
+    }
+
+    pub fn is_streamed_in(&self, player: &Player) -> bool {
+        functions::actors::IsActorStreamedIn(self, player)
+    }
+
+    pub fn set_virtual_world(&self, virtual_world: isize) {
+        functions::actors::SetActorVirtualWorld(self, virtual_world)
+    }
+
+    pub fn get_virtual_world(&self) -> isize {
+        functions::actors::GetActorVirtualWorld(self)
+    }
+
+    pub fn apply_animation(&self, animation_data: AnimationData) {
+        functions::actors::ApplyActorAnimation(
+            self,
+            &animation_data.get_animation_library(),
+            &animation_data.get_name(),
+            animation_data.delta,
+            animation_data.looping,
+            animation_data.lockX,
+            animation_data.lockY,
+            animation_data.freeze,
+            animation_data.time,
+        )
+    }
+
+    pub fn clear_animations(&self) -> bool {
+        functions::actors::ClearActorAnimations(self)
+    }
+
+    pub fn set_pos(&self, pos: Vector3) {
+        functions::actors::SetActorPos(self, pos)
+    }
+
+    pub fn getpos(&self) -> Vector3 {
+        let mut position = Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        };
+        functions::actors::GetActorPos(self, &mut position);
+        position
+    }
+
+    pub fn setfacing_angle(&self, angle: f32) {
+        functions::actors::SetActorFacingAngle(self, angle)
+    }
+
+    pub fn getfacing_angle(&self) -> f32 {
+        functions::actors::GetActorFacingAngle(self)
+    }
+
+    pub fn sethealth(&self, health: f32) {
+        functions::actors::SetActorHealth(self, health)
+    }
+
+    pub fn gethealth(&self) -> f32 {
+        functions::actors::GetActorHealth(self)
+    }
+
+    pub fn setinvulnerable(&self, invulnerable: bool) {
+        functions::actors::SetActorInvulnerable(self, invulnerable)
+    }
+
+    pub fn isinvulnerable(&self) -> bool {
+        functions::actors::IsActorInvulnerable(self)
+    }
+
+    pub fn is_valid(&self) -> bool {
+        functions::actors::IsValidActor(self)
+    }
+
+    pub fn set_skin(&self, skin: isize) {
+        functions::actors::SetActorSkin(self, skin)
+    }
+
+    pub fn get_skin(&self) -> isize {
+        functions::actors::GetActorSkin(self)
+    }
+
+    pub fn get_animation(&self) -> AnimationData {
+        let (
+            mut animation_library,
+            mut animation_name,
+            mut delta,
+            mut animloop,
+            mut lock_x,
+            mut lock_y,
+            mut freeze,
+            mut time,
+        ) = Default::default();
+
+        functions::actors::GetActorAnimation(
+            self,
+            &mut animation_library,
+            &mut animation_name,
+            &mut delta,
+            &mut animloop,
+            &mut lock_x,
+            &mut lock_y,
+            &mut freeze,
+            &mut time,
+        );
+
+        AnimationData::new(
+            delta,
+            animloop,
+            lock_x,
+            lock_y,
+            freeze,
+            time,
+            &animation_library,
+            &animation_name,
+        )
+    }
+
+    pub fn get_spawn_info(&self) -> ActorSpawnData {
+        let mut spawn_data = ActorSpawnData::default();
+        functions::actors::GetActorSpawnInfo(self, &mut spawn_data);
+        spawn_data
+    }
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, Default)]
 #[repr(C)]
 pub struct ActorSpawnData {
     position: Vector3,
--- omprs-gdk/src/functions/actors.rs
+++ omprs-gdk/src/functions/actors.rs
@@ -1,25 +1,32 @@
+use crate::actors::*;
+use crate::{
+    players::Player,
+    vector::Vector3,
+};
 use omprs_codegen::native;
+use std::ffi::c_void;
 
-native!(CreateActor,skin: isize, x: f32, y: f32, z: f32, angle: f32,-> isize);
-native!(DestroyActor,actorid: isize,->bool);
-native!(IsActorStreamedIn,actorid: isize, playerid: isize,->bool);
-native!(SetActorVirtualWorld,actorid: isize, virtualWorld: isize,->bool);
-native!(GetActorVirtualWorld,actorid: isize,-> isize);
-native!(ApplyActorAnimation,actorid: isize, animationLibrary: str, animationName: str, delta: f32, animloop: bool, lockX: bool, lockY: bool, freeze: bool, time: isize,->bool);
-native!(ClearActorAnimations,actorid: isize,->bool);
-native!(SetActorPos,actorid: isize, x: f32, y: f32, z: f32,->bool);
-native!(GetActorPos,actorid: isize, x: mut f32, y: mut f32, z: mut f32,->bool);
-native!(SetActorFacingAngle,actorid: isize, angle: f32,->bool);
-native!(GetActorFacingAngle,actorid: isize, angle: mut f32,->bool);
-native!(SetActorHealth,actorid: isize, health: f32,->bool);
-native!(GetActorHealth,actorid: isize, health: mut f32,->bool);
-native!(SetActorInvulnerable,actorid: isize, invulnerable: bool,->bool);
-native!(IsActorInvulnerable,actorid: isize,->bool);
-native!(IsValidActor,actorid: isize,->bool);
-native!(SetActorSkin,actorid: isize, skin: isize,->bool);
-native!(GetActorSkin,actorid: isize,-> isize);
-native!(GetActorAnimation,actorid: isize, animationLibrary: mut str, animationName: mut str, delta: mut f32, animloop: mut bool, lockX: mut bool, lockY: mut bool, freeze: mut bool, time: mut usize,->bool);
-native!(GetActorSpawnInfo,actorid: isize, skin: mut isize, x: mut f32, y: mut f32, z: mut f32, angle: mut f32,->bool);
+native!(CreateActor, skin: isize, pos:Vector3, angle: f32, -> struct Actor);
+native!(DestroyActor, actor: struct Actor);
+native!(IsActorStreamedIn, actor: struct Actor, player: struct Player, -> bool);
+native!(SetActorVirtualWorld, actor: struct Actor, virtualWorld: isize);
+native!(GetActorVirtualWorld, actor: struct Actor, -> isize);
+native!(ApplyActorAnimation,actor: struct Actor, animationLibrary: str, animationName: str, delta: f32, animloop: bool, lockX: bool, lockY: bool, freeze: bool, time: usize);
+native!(ClearActorAnimations, actor: struct Actor, -> bool);
+native!(SetActorPos, actor: struct Actor, pos:Vector3);
+native!(GetActorPos, actor: struct Actor, position:mut Vector3);
+native!(SetActorFacingAngle, actor: struct Actor, angle: f32);
+native!(GetActorFacingAngle, actor: struct Actor, -> f32);
+native!(SetActorHealth, actor: struct Actor, health: f32);
+native!(GetActorHealth, actor: struct Actor, -> f32);
+native!(SetActorInvulnerable, actor: struct Actor, invulnerable:bool);
+native!(IsActorInvulnerable, actor: struct Actor, -> bool);
+native!(IsValidActor, actor: struct Actor, -> bool);
+native!(SetActorSkin, actor: struct Actor, skin: isize);
+native!(GetActorSkin, actor: struct Actor, -> isize);
+native!(GetActorAnimation,actor: struct Actor, animationLibrary: mut str, animationName: mut str, delta: mut f32, animloop: mut bool, lockX: mut bool, lockY: mut bool, freeze: mut bool, time: mut usize,->bool);
+native!(GetActorSpawnInfo, actor: struct Actor, spawnData:mut ActorSpawnData);
+//native!(GetActorsComponent, -> struct ActorsComponent);
 
 pub fn load_actor_functions() {
     load_function!(CreateActor);
