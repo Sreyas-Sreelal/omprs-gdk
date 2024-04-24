8ad9d673125a1ca53f82d1471afc3c721f0a6d8a
SyS <sreyassreelal@gmail.com>
2024-04-24 13:02:31 +0530

    keep it an expression as the struct might be needed to initialise

--- omprs-gdk/src/runtime.rs
+++ omprs-gdk/src/runtime.rs
@@ -4,7 +4,7 @@ pub static mut Runtime: Option<Box<dyn Events + 'static>> = None;
 
 #[macro_export]
 macro_rules! register {
-    ($name:ident) => {
+    ($name:expr) => {
         unsafe {
             omprs_gdk::Runtime = Some(Box::new($name));
         }
