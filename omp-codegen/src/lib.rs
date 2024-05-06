7d05a3f32d1a076aa4fec60f050f086dc5a09c16
SyS <sreyassreelal@gmail.com>
2024-05-06 19:49:27 +0530

    code gen docs

--- omp-codegen/src/lib.rs
+++ omp-codegen/src/lib.rs
@@ -4,16 +4,22 @@ mod callbacks;
 mod entrypoint;
 mod natives;
 
+/// Creates a callback function for the component to call
 #[proc_macro]
 pub fn callback(args: TokenStream) -> TokenStream {
     callbacks::create_callback(args)
 }
 
+/// Creates an entry point function for the component to call when it loads
+/// Entry point function is necessary, all the function address is calculated and initialised here
 #[proc_macro_attribute]
 pub fn main(args: TokenStream, input: TokenStream) -> TokenStream {
     entrypoint::create_main(args, input)
 }
 
+/// Creates a userfunction that calls the native function in component
+/// str - for string data 
+/// struct - for open.mp object pointers
 #[proc_macro]
 pub fn native(args: TokenStream) -> TokenStream {
     natives::create_native(args)
