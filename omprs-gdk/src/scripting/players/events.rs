1e2fe79066f036d49435cf418e34683d45b1aa59
SyS <sreyassreelal@gmail.com>
2024-05-06 10:01:02 +0530

    add rest of player events

--- omprs-gdk/src/scripting/players/events.rs
+++ omprs-gdk/src/scripting/players/events.rs
@@ -1,6 +1,8 @@
-use super::{BodyPart, Player, PlayerClickSource, PlayerState};
+use super::{BodyPart, Player, PlayerBulletData, PlayerClickSource, PlayerState};
+use crate::objects::{Object, PlayerObject};
 use crate::types::network::PeerDisconnectReason;
 use crate::types::vector::Vector3;
+use crate::vehicles::Vehicle;
 use omprs_codegen::callback;
 
 callback!(OnPlayerConnect, player:Player);
@@ -22,3 +24,10 @@ callback!(OnPlayerClickMap, player:Player, pos:Vector3);
 callback!(OnPlayerClickPlayer, player:Player, clicked:Player, source:PlayerClickSource);
 callback!(OnClientCheckResponse, player:Player, action_type:isize, address:isize, results:isize);
 callback!(OnPlayerUpdate, player:Player,now:isize,->bool);
+callback!(onPlayerShotMissed, player: Player, bulletData: PlayerBulletData,-> bool);
+callback!(onPlayerShotPlayer, player: Player, target: Player, bulletData: PlayerBulletData,-> bool);
+callback!(onPlayerShotVehicle, player: Player, target: Vehicle, bulletData: PlayerBulletData,-> bool);
+callback!(onPlayerShotObject,player: Player, target: Object, bulletData: PlayerBulletData,-> bool);
+callback!(onPlayerShotPlayerObject,player: Player, target: PlayerObject, bulletData: PlayerBulletData,-> bool);
+callback!(onPlayerScoreChange,player: Player, score: isize);
+callback!(onPlayerNameChange,player: Player, oldName: String);
